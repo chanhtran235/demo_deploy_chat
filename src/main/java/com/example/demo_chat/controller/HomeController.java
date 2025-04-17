0c4b90799b9b52458b880a19ed42e1ae4a22fcb7
chanhtv235 <chanhtv235@gmail.com>
2025-04-17 17:20:43 +0700

    add db

--- src/main/java/com/example/demo_chat/controller/HomeController.java
+++ src/main/java/com/example/demo_chat/controller/HomeController.java
@@ -1,10 +1,18 @@
 package com.example.demo_chat.controller;
 
+import com.example.demo_chat.model.AppUser;
+import com.example.demo_chat.service.IAppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private IAppUserService appUserService;
     @GetMapping("/login")
     public String login() {
         return "login";
@@ -20,7 +28,9 @@ public class HomeController {
         return "user-profile";
     }
     @GetMapping("/home")
-    public String home(){
+    public String home(Model model){
+        List<AppUser> list = appUserService.findAll();
+        model.addAttribute("list", list);
         return "home";
     }
 }
---
+++ src/main/java/com/example/demo_chat/service/AppUserService.java
@@ -0,0 +1,18 @@
+package com.example.demo_chat.service;
+
+import com.example.demo_chat.model.AppUser;
+import com.example.demo_chat.repository.IAppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AppUserService implements IAppUserService {
+    @Autowired
+    private IAppUserRepository appUserRepository;
+    @Override
+    public List<AppUser> findAll() {
+        return appUserRepository.findAll();
+    }
+}
