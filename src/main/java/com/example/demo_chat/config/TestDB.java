3b85d17fcd574bdcfb1274683dfa45e72ad015b2
chanhtv235 <chanhtv235@gmail.com>
2025-04-17 21:13:19 +0700

    update bien moi truong1

---
+++ src/main/java/com/example/demo_chat/config/TestDB.java
@@ -0,0 +1,23 @@
+package com.example.demo_chat.config;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+public class TestDB {
+    @Value("${JDBC_DATABASE_URL}")
+    private String jdbcUrl;
+
+    @Value("${DATABASE_USERNAME}")
+    private String dbUsername;
+
+    @Value("${DATABASE_PASSWORD}")
+    private String dbPassword;
+
+    @PostConstruct
+    public void logDatabaseDetails() {
+        System.out.println("============= KIỂM TRA BIẾN MÔI TRƯỜNG================= =====");
+        System.out.println("JDBC URL: " + jdbcUrl);
+        System.out.println("Database Username: " + dbUsername);
+        System.out.println("Database Password: " + dbPassword);
+        System.out.println("====================================");
+    }
+}
